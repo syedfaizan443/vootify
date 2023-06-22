639dfab3c17e10cf2a00d26673f8f499b42f7756
syedfaizan443 <syedfaizan443@gmail.com>
2023-06-22 14:07:29 +0100

    footer update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
   return (
 <footer className={` min-w-[20rem] dark:bg-gray-900`}>
     <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-      <span className="text-sm  sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.
+      <span className="text-sm  sm:text-center dark:text-gray-400">© 2023 vootify. All Rights Reserved.
     </span>
     <ul className="flex flex-wrap items-center mt-3 text-sm font-medium  dark:text-gray-400 sm:mt-0">
         <li>
