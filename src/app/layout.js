8f437aff0ad0387f0841778a325ebd721737842a
syedfaizan443 <syedfaizan443@gmail.com>
2023-06-23 07:30:34 +0100

    fje

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,6 +13,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {
+          (process.env.NODE_ENV === "production" && (
+            <script async src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID}`}
+     crossorigin="anonymous"></script>
+          ))
+        }
+      </head>
       <body className={inter.className}>
       <ThemeProviderr>
       <Navbar></Navbar>
